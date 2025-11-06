d9a625e5c004721b62cca0499296ca261999e1a8
ttoksas <58305934+ttunahanoksas@users.noreply.github.com>
2025-11-06 10:32:21 +0100

    Update first_task.c

--- first_task.c
+++ first_task.c
@@ -1,20 +1,11 @@
-/* Task description:
-Write a program that asks the user to enter a word, stores it in a string, and prints it letter by letter vertically. For example if you give „Word”, the result should be:
-
-W
-o
-r
-d
-
-You can assume that the word is never longer than 99 characters.
-
-*/
-
-
 #include <stdio.h>
 
 int main(){
-
-
-  return 0;
+char word[100];  
+printf("Enter a word: ");
+scanf("%99s", word);  
+for (int i = 0; word[i] != '\0'; i++) {
+  printf("%c\n", word[i]);
+}
+return 0;
 }
